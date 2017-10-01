2bb185841895899d4e3a4c8b5562a9779cc59410
Blaine Richardson <blainerrichardson@gmail.com>
2017-10-01 01:45:58 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeBirthdate < ActiveRecord::Migration
   def change
-    change_column :students, :birthdate, datetime
+    change_column :students, :birthdate, :datetime
   end
 end
